c3380dc1dd3913234d6f6cac8c074dcaff9e2863
deelight <akingbenidavidoyin@gmail.com>
2024-04-25 15:59:52 +0000

    task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- File to create function that does safe division.
+-- Change Delimiter
+DELIMITER $$
+
+-- DEFINE FUNCITON
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS float
+BEGIN
+	IF b = 0 THEN RETURN 0
+	END IF;
+	RETURN a / b;
+END$$
+
+-- reset delimiter
+DELIMITER ;
