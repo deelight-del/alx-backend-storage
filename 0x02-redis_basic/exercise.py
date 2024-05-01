604a695314eb1735d1ab9b7be35c4c6b199d5033
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-01 19:16:18 +0000

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ implements the redis-py package, imported as redis"""
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Any, Optional
 
 
 class Cache:
@@ -21,3 +21,22 @@ class Cache:
         random_string = str(uuid.uuid4())
         self._redis.set(random_string, data)
         return random_string
+
+    def get(self, key: str,
+            fn: Optional[Callable[[bytes], Optional[Any]]] = None) -> Optional[Any]:
+        """Defining the get method."""
+        if self._redis.get(key) is not None:
+            return (
+                self._redis.get(key)
+                if fn is None
+                else fn(self._redis.get(key))
+            )
+        return None
+
+    def get_str(self, key: str):
+        """Use Cache.get to convert to str"""
+        self.get(key, lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str):
+        """Use Cache.get to convert to int"""
+        self.get(key, int)
