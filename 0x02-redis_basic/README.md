65ee51a31667562e87e925943b8cbcfd158b7b43
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-01 17:41:08 +0000

    Task 0

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,6 @@
+# 0x02. Redis basic
+
+In this directory, we explore the basis and advanced usage of
+the Redis NoSQL database.
+We mainly use the redis-py API of python to interact
+with the redis-server as needed.
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""In this module, we create a Cache class that
+implements the redis-py package, imported as redis"""
+
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """A Cache class expected to work as a caching system"""
+    def __init__(self):
+        """The initialization method"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str | bytes | int | float]):
+        """store method that takes a data
+        argument and returns a string"""
+        random_string = uuid.uuid4()
+        self._redis.set(random_string, data)
+        return random_string
