72982bd656a14466b465b661d66434b5cb6dc976
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-01 17:57:52 +0000

    Task, fix mypy issues

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,9 +15,10 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str | bytes | int | float]):
+    def store(self, data: Union[str, bytes, int, float]):
         """store method that takes a data
         argument and returns a string"""
         random_string = uuid.uuid4()
         self._redis.set(random_string, data)
+        # self._redis.bgsave()
         return random_string
